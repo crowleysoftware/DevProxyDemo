bf8ebe70c5e6783cb389a0039ac8f67f94c4f53e
Bob <bob@crowley.software>
2025-12-04 17:14:07 -0500

    updating for v2

--- 2_CustomRandomError/requests.ps1
+++ 2_CustomRandomError/requests.ps1
@@ -1,7 +1,11 @@
 
 # responses will be chosen from the error response that is configured for the /users endpoint
 Invoke-WebRequest -Uri "https://jsonplaceholder.typicode.com/users/1" -Method Get -SkipHttpErrorCheck
+$c1 = Invoke-WebRequest -Uri "https://jsonplaceholder.typicode.com/users/1" -Method Get -SkipHttpErrorCheck
+Write-Output $c1.Content
 
 # responses will be chosen from the error response that is configured for the /todos endpoint
-Invoke-WebRequest -Uri "https://jsonplaceholder.typicode.com/todos/1" -Method Get -SkipHttpErrorCheck
+$c = Invoke-WebRequest -Uri "https://jsonplaceholder.typicode.com/todos/1" -Method Get -SkipHttpErrorCheck
+
+Write-Output $c.Content
     
